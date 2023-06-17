c6837c4866a5efc58bc17ecf5d05c8d9ebc7c8b5
Dohuy5360 <loldh321@gmail.com>
2023-06-17 17:52:45 +0700

    Test: Change branch rule

--- src/main/java/org/example/selenium/projects/school/LearningGathering.java
+++ src/main/java/org/example/selenium/projects/school/LearningGathering.java
@@ -12,12 +12,17 @@ import java.util.Properties;
 
 public class LearningGathering {
 
-    static  Driver driver = new Driver();
+    static Driver driver = new Driver();
     static Properties configFile = new PropertiesFile("config.properties").read();
+
     public static void main(String[] args) {
-        new Omayo(driver.useChrome(),"http://omayo.blogspot.com/p/page7.html").downloadFile().quit();
-        // new Petstore(driver.useChrome(), "https://petstore.octoperf.com/actions/Catalog.action", configFile).login().signOut().quit();
-        // new Globalsqa(driver.useChrome(), "https://www.globalsqa.com/angularJs-protractor/BankingProject/#/login",configFile).login().quit();
-        // new Elearning(driver.useChrome(),"https://elearning.vanlanguni.edu.vn/",configFile).login().openDashboard();
+        new Omayo(driver.useChrome(), "http://omayo.blogspot.com/p/page7.html").downloadFile().quit();
+        // new Petstore(driver.useChrome(),
+        // "https://petstore.octoperf.com/actions/Catalog.action",
+        // configFile).login().signOut().quit();
+        // new Globalsqa(driver.useChrome(),
+        // "https://www.globalsqa.com/angularJs-protractor/BankingProject/#/login",configFile).login().quit();
+        // new
+        // Elearning(driver.useChrome(),"https://elearning.vanlanguni.edu.vn/",configFile).login().openDashboard();
     }
 }
