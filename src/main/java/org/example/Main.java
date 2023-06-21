f6c53a3a519b633a9ad1804803bc981697150136
Đỗ Huy <80961056+DoHuy5360@users.noreply.github.com>
2023-06-22 02:14:29 +0700

    Dohuy (#5)
    * Test: Change branch rule
    
    * Change: Restructure Project Pattern and completed Exercise 02
    
    * Add: Logout action
    
    * Change: Rename folder
    
    * Add: Read Excel files

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,15 +1,5 @@
 package org.example;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.edge.EdgeDriver;
-import org.openqa.selenium.edge.EdgeOptions;
-
-import java.util.HashMap;
-
 public class Main {
     public static void main(String[] args) {
 
